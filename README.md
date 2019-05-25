345c7491924170c7ecd98efc95b241f2bddf8fc6
Leslie Boardman <leslie@araitanga.com>
2019-05-26 10:41:42 +1200

    starting to look at image rotation

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 ## TODO
  - Read EXIF and rotate image if required
+   https://stackoverflow.com/questions/5905868/how-to-rotate-jpeg-images-based-on-the-orientation-metadata
  - Copy in other face detect stuff.
  - Display help if no options given (currently executes but later will be able to choose stuff.
+ 
+ Many thanks to StackOverflow.com without whose help this would not have been possible.
  
--- src/main/java/com/araitanga/timelapseFace/App.java
+++ src/main/java/com/araitanga/timelapseFace/App.java
@@ -7,7 +7,14 @@ import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.OptionBuilder;
 import org.apache.commons.cli.ParseException;
+
+import com.drew.imaging.ImageInformation;
+import com.drew.imaging.Metadata;
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.imaging.ImageProcessingException;
+
 import java.io.IOException;
+import java.io.File;
 
 /**
  * Timelapse Face processor
@@ -48,4 +55,37 @@ public class App
 
 		
     }
+	public static class ImageInformation {
+		public final int orientation;
+		public final int width;
+		public final int height;
+
+		public ImageInformation(int orientation, int width, int height) {
+			this.orientation = orientation;
+			this.width = width;
+			this.height = height;
+		}
+
+		public String toString() {
+			return String.format("%dx%d,%d", this.width, this.height, this.orientation);
+		}
+	}
+	
+	public static ImageInformation readImageInformation(File imageFile)  throws IOException, MetadataException, ImageProcessingException {
+    Metadata metadata = ImageMetadataReader.readMetadata(imageFile);
+    Directory directory = metadata.getFirstDirectoryOfType(ExifIFD0Directory.class);
+    JpegDirectory jpegDirectory = metadata.getFirstDirectoryOfType(JpegDirectory.class);
+
+    int orientation = 1;
+    try {
+        orientation = directory.getInt(ExifIFD0Directory.TAG_ORIENTATION);
+    } catch (MetadataException me) {
+        logger.warn("Could not get orientation");
+    }
+    int width = jpegDirectory.getImageWidth();
+    int height = jpegDirectory.getImageHeight();
+
+    return new ImageInformation(orientation, width, height);
+}
+
 }
